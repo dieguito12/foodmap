32ef5b2af80b5532db3012c8f14d642f941062dd
Hector Aristy <rafelito266@gmail.com>
2016-09-24 21:30:03 -0400

    Feature of menu detail

---
+++ FoodMap/src/MenuDetail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class MenuDetail extends Component {
+    render() {
+        return (
+            <div>
+            <p>Descripcion</p>
+            <p>Rating</p>
+            <p>Direccion</p>
+            <p>Telefono(s)</p>
+            <p>E-mail</p>
+            <p>Horario regular</p>
+            </div>
+        );
+
+    }
+}
+
+export default MenuDetail;
--- FoodMap/src/index.js
+++ FoodMap/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MenuContainer from './MenuContainer';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <MenuContainer />,
   document.getElementById('root')
 );
