3326a4cd53df6d38eb227aab082fe06fd03f7607
Diego Mena <Diego Mena>
2016-10-08 00:01:46 -0400

    Do some fixes

--- FoodMap/src/App.js
+++ FoodMap/src/App.js
@@ -1,32 +1,18 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import logo from './logo.svg';
 import './App.css';
 import LoginFormContainer from './views/login/containers/LoginFormContainer';
 import Auth from './auth/Auth';
 import RestContainer from './views/restaurants/containers/RestContainer';
 import HeaderBar from './views/layout/components/HeaderBar';
 
-var loginEndpoint = 'http://159.203.191.142:8080/login';
 
 class App extends Component {
-
-  state = {
-    title: 'Inicio de Sesión'
-  }
-
-  handleOnRegisterLinkClicked = () => {
-    this.setState({title: 'Registrarse'});
-  }
 
   render() {
     if (Auth.loggedUser() == null) {
       return (
         <div className="container">
-          <LoginFormContainer
-            title={this.state.title}
-            onRegisterLinkClicked={this.handleOnRegisterLinkClicked}
-            endpointUrl={loginEndpoint} />
+          <LoginFormContainer />
         </div>
       );
     } else {
--- FoodMap/src/views/layout/components/HeaderBar.js
+++ FoodMap/src/views/layout/components/HeaderBar.js
@@ -6,7 +6,18 @@ var logoStyle = {
     height: '50px'
 }
 
+var mobileLogoutStyle = {
+    color: '#26a69a',
+    fontSize: '16px',
+    textDecoration: 'none'
+}
+
+var noDecoration = {
+    textDecoration: 'none'
+}
+
 class HeaderBar extends Component {
+
     handleLogout = (e) => {
         Auth.unsetUser();
         location.reload();
@@ -17,29 +28,32 @@ class HeaderBar extends Component {
             <div className="navbar-fixed">
                 <ul id="dropdown1" className="dropdown-content">
                     <li className="divider"></li>
-                    <li><a onClick={this.handleLogout}>Logout</a></li>
+                    <li><a style={noDecoration} onClick={this.handleLogout}>Logout</a></li>
+                </ul>
+                <ul id="mobile-demo" className="side-nav">
+                    <li><a style={mobileLogoutStyle} href="#" onClick={this.handleLogout}>Logout</a></li>
                 </ul>
                 <nav id="header-bar">
                     <div className="nav-wrapper">
                         <a className="brand-logo center" href="#" >
                             <img alt="Brand" style={logoStyle} src="/assets/logo.png"/>
                         </a>
-                        <a data-activates="mobile-demo" className="button-collapse">
+                        <a
+                            data-activates="mobile-demo" href="#"
+                            className="button-collapse">
                             <i className="material-icons">menu</i>
                         </a>
                         <ul className="right hide-on-med-and-down">
                             <li>
                                 <a
                                     className="dropdown-button"
+                                    data-beloworigin="true"
+                                    style={noDecoration}
                                     data-activates="dropdown1">
                                     Bienvenido: {this.props.username}
                                     <i className="material-icons left">perm_identity</i>
                                 </a>
                             </li>
-                        </ul>
-                        <ul className="side-nav" id="mobile-demo">
-                            <li><p href="collapsible.html">{this.props.username}</p></li>
-                            <li><a onClick={this.handleLogout}>Logout</a></li>
                         </ul>
                     </div>
                 </nav>
